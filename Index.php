bf3932196292ab348c4cfd3dd8ef41ada5da8adb
George Hall <georgeehall@hotmail.co.uk>
2023-02-10 16:40:59 +0000

    Continuing with layout

--- Index.php
+++ Index.php
@@ -359,6 +359,7 @@
             </div><!-- Col 3 End -->
         </div><!-- Row End -->
         <h2></h2>
+        <h3>Our Top Destinations</h3>
         <!-- Footer Start -->
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
             style="position: relative; z-index: 1;">
